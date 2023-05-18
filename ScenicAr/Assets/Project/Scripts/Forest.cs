0de0739ad4bd302949186a2f8920d6a920b5a6d3
nuclearmayhem1 <53118215+nuclearmayhem1@users.noreply.github.com>
2023-05-18 18:45:58 +0200

    working app

--- ScenicAr/Assets/Project/Scripts/Forest.cs
+++ ScenicAr/Assets/Project/Scripts/Forest.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class Forest : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public TerrainGenerator terrainGenerator;
+    public List<GameObject> spawnedObjects = new List<GameObject>();
+    public int spawnCount = 1;
+    public Transform spawnPoint;
+
+    private List<GameObject> instantiated = new List<GameObject>();
+
+    public void GenerateForest()
     {
-        
+        foreach (GameObject gameObject in instantiated)
+        {
+            Destroy(gameObject);
+        }
+
+        terrainGenerator.seed = Random.Range(-10000, 10000);
+        terrainGenerator.GenerateNoisemap();
+        terrainGenerator.GenerateColormap();
+        terrainGenerator.GenerateMesh();
+
+        Debug.Log("found forest");
+
+        for (int i = 0; i < spawnCount; i++)
+        {
+            Vector3 rayOrigin = spawnPoint.position + spawnPoint.rotation * new Vector3(Random.Range(-1f, 1f), 0, Random.Range(-1f, 1f));
+            Ray ray = new Ray(rayOrigin, Vector3.down);
+            if (Physics.Raycast(ray, out RaycastHit hitInfo))
+            {
+                GameObject toSpawn = spawnedObjects[Random.Range(0, spawnedObjects.Count)];
+                instantiated.Add(Instantiate(toSpawn, hitInfo.point, Quaternion.identity, spawnPoint));
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
